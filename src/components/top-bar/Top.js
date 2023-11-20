52217deed5d73160ba788d1cf1ec8c31ef78a80d
Nitish Sharma <rsony8884@gmail.com>
2023-11-20 08:41:30 +0530

    Top bar added

--- src/components/top-bar/Top.js
+++ src/components/top-bar/Top.js
@@ -1,7 +1,16 @@
+import user from "../../icons/user-icon.png"
 const Top = () => {
     return (
-        <div className="w-full h-11 bg-slate-100 sticky top-0">
-            <p>hii</p>
+        <div className="w-full h-12 flex justify-end bg-slate-100 sticky top-0 px-2">
+
+            <button className="flex flex-col items-center justify-center">
+
+                <img src={user} className="w-5 h-6 mt-2"></img>
+
+                <h1 className="text-lg text-ellipsis">Username</h1>
+
+            </button>
+
         </div>
     );
 }
